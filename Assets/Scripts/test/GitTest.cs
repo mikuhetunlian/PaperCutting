1e3859c4c16f7b9df7ce79d097f74f82203ba659
mikuhetunlian <1572930014@qq.com>
2022-09-08 17:14:39 +0800

    GitTestCommit

---
+++ Assets/Scripts/test/GitTest.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GitTest : MonoBehaviour
+{
+   
+    void Start()
+    {
+        Debug.Log("GitTest");
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
